ae9ed10e1e12133d71dabdcd6ce7a10268d41869
Andriamisa Nayann <99319983+nayannou@users.noreply.github.com>
2023-01-04 23:50:09 +0100

    Add files via upload

---
+++ Test Bootstrap/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>BDE USPN</title>
+    <link rel="shortcut icon" href="Images/Logo_bde">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <link rel="stylesheet" href="css/style.css">
+    <!-- JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</head>
+    <!-- Header -->
+    <header>
+        <nav class="navbar fixed-top navbar-expand-md">
+            <div class="container-fluid">
+                <!-- LOGO -->
+                <div class="d-flex">
+                    <a class="navbar-brand" href="">
+                        <img src="Images/Logo_bde" height="80" width="80"> 
+                    </a>
+                    <ul class="navbar-nav d-flex align-items-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Accueil</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown">Commande</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="">Nouvelle Commande</a></li>
+                                <li><a class="dropdown-item" href="">Retour Commande</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown">Inventaire</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="">Stock/Réaprovisionnement</a></li>
+                                <li><a class="dropdown-item" href="">Ajout d'article</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown">Comptabilité</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="">Historique des ventes</a></li>
+                                <li><a class="dropdown-item" href="">Bilan</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Gestion des comptes</a></li>
+                    </ul>
+                </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><img src="Images/icone.png" height="25" width="25"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><img src="Images/cloche.png" height="25" width="25"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><img src="Images/drapeau" height="15" width="25"></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Fin Header -->
